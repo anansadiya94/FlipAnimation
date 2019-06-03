ec82ae6328b98c3e3671be8cb8d77274c5409f30
Anan Sadiya <anan.sadiya@e-campus.uab.cat>
2019-06-03 10:50:31 +0200

    Initial project commit

---
+++ FlipAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlipAnimation
+//
+//  Created by Anan Sadiya on 03/06/2019.
+//  Copyright © 2019 Anan Sadiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
